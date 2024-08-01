e9926db90a5709bd9221ee85f4d5c2ae730329e8
S-SIRIUS <sirius5b8b@gmail.com>
2024-08-01 19:18:09 +0900

    [feat] Streaming Baseline

---
+++ Prompt/seed.py
@@ -0,0 +1,7 @@
+def seed(text, user_info):
+    prompt = f'''
+        {text}
+        {user_info} # user_info 는 딕셔너리 형태로 만들 예정
+    
+    '''
+    return prompt
--- RAG/core_Rag.py
+++ RAG/core_Rag.py
@@ -17,6 +17,9 @@ import chromadb
 from llama_index.vector_stores.chroma import ChromaVectorStore
 
 from Chat.core_Chat import *
+from Prompt.seed import seed
+from Prompt.sprout import sprout
+from Prompt.tooki import tooki
 
 # llm 모델 정보
 llm = OpenAI(temperature=0.2, model="gpt-4")
@@ -35,20 +38,38 @@ storage_context = StorageContext.from_defaults(vector_store = vector_store)
 index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, embed_model = embed_model)
 
 # 쿼리엔진 생성
-query_engine = index.as_query_engine(llm=llm)
+query_engine = index.as_query_engine(streaming=True, llm=llm) # 스트리밍으로 반환하도록 설정
 
-def core_Rag(message):
+async def core_Rag(message, user_info):
 
     # 이전 대화 요약
     history_summary = summarize_history()
     print(history_summary)
-    # 요약된 대화 맥락과 현재 메시지를 결합한 쿼리 작성
-    combined_message = f"Preveous summary: {history_summary}\nUser query:{message}"
+
+    # 사용자 수준 별 다른 프롬프트 제공 (사용자에 대한 정보를 반영하여 맞춤형 답변을 준다)
+    '''
+    if user_info["level"] == "seed":
+        prompt = seed(message, user_info)
+    elif user_info["level"] == "sprout":
+        prompt = sprout(message, user_info)
+    else:
+        prompt = tooki(message, user_info)
+    '''
+    prompt = message
+    # 요약된 대화 맥락과 현재 프롬프트를 결합한 쿼리 작성
+    print("현재 맥락", history_summary)
+    combined_message = f"Preveous summary: {history_summary}\nUser query:{prompt}"
 
 
-    response = query_engine.query(combined_message)
+    streaming_response = query_engine.query(combined_message)
 
-    return response
+    print("현재 뽑히고 있는:")
+    response_text = ""
+    for text in streaming_response.response_gen:
+        print(text)
+        response_text += text
+
+    return response_text
 
 '''
 # 문서를 노드 단위로 분할
