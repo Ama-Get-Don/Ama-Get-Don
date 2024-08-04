5799dea7d901427958e51ec957720fa888e67343
eowwo9 <owwo1@naver.com>
2024-08-04 18:30:45 +0900

    feat: header color and aad logo pic

--- tookie/src/Header/index.tsx
+++ tookie/src/Header/index.tsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import { Layout, Menu } from 'antd';
 import { HomeOutlined, InfoCircleOutlined, PhoneOutlined } from '@ant-design/icons';
-import { styled } from 'styled-components';
+import styled from 'styled-components';
+import tookieLogo from '../assets/tookie_logo.png'; // 이미지 import
 
 const { Header } = Layout;
 
 const AppHeader: React.FC = () => {
   return (
     <HeaderStyled>
-      <Logo>Tookie</Logo>
+      <LogoWrapper>
+        <LogoImage src={tookieLogo} alt="Tookie Logo" />
+      </LogoWrapper>
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
         <Menu.Item key="1" icon={<HomeOutlined />}>
           Home
@@ -21,7 +25,6 @@ const AppHeader: React.FC = () => {
       </Menu>
     </HeaderStyled>
   )
-
 }
 
 export default AppHeader;
@@ -30,10 +33,14 @@ const HeaderStyled = styled(Header)`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  background-color: white;
 `;
 
-const Logo = styled.div`
-  color: white;
-  font-size: 1.5rem;
-  font-weight: bold;
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoImage = styled.img`
+  height: 35px; // 로고 크기 조절 (필요에 따라 조정)
 `;
