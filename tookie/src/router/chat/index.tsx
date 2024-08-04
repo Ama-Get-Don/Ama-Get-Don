58c6c904989ebdb0df9e2bdabe02639ab791e1d8
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2024-08-04 15:26:14 +0900

    feat: create route and page

---
+++ tookie/src/router/chat/index.tsx
@@ -0,0 +1,14 @@
+import { RouteObject } from "react-router";
+import { Chat } from "../../pages/chat";
+
+const chatRoute: RouteObject = {
+    path: 'chat',
+    children: [
+        {
+            path: '/',
+            element: <Chat />
+        }
+    ]
+}
+
+export default chatRoute;
