0bb34713360134c6cabc5594354093507619c296
sangeun815 <grace0857@naver.com>
2024-08-07 06:28:50 +0900

    change Survey file name

--- tookie/src/router/signup/index.tsx
+++ tookie/src/router/signup/index.tsx
@@ -1,7 +1,7 @@
 import { RouteObject } from "react-router-dom";
 import { Terms } from "../../pages/signup/Terms";
 import { Info } from "../../pages/signup/Info";
-import {Survey} from "../../pages/signup/Survey";
+import {KnowledgeLevelSurvey} from "../../pages/signup/KnowledgeLevelSurvey";
 import { SignUpComplete } from "../../pages/signup/SignUpComplete";
 
 const signUpRoute: RouteObject = {
@@ -16,8 +16,8 @@ const signUpRoute: RouteObject = {
             element: <Info />
         },
         {
-            path: 'survey', 
-            element: <Survey />
+            path: 'knowledge_level_survey', 
+            element: <KnowledgeLevelSurvey />
         },
         {
             path: 'complete', 
