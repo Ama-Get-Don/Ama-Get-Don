d497ed9526aee5aee2b5f277723d128bafdc36a3
sangeun815 <grace0857@naver.com>
2024-08-09 05:17:21 +0900

    change user_schema.py

--- tookie-BE/user/user_schema.py
+++ tookie-BE/user/user_schema.py
@@ -1,29 +1,26 @@
-# user_schema.py
-from pydantic import BaseModel, EmailStr, field_validator
-from pydantic_core.core_schema import FieldValidationInfo
-import datetime
+from pydantic import BaseModel, EmailStr, validator
+from enum import Enum
 
+class GenderEnum(str, Enum):
+    남자 = "남자"
+    여자 = "여자"
 
 class UserCreate(BaseModel):
     name: str
-    password1: str
-    password2: str
+    password: str
     email: EmailStr
+    birthdate: str  # 생년월일
+    phone_number: str
+    gender: GenderEnum
 
-    @field_validator('name', 'password1', 'password2', 'email')
+    @validator('name', 'password', 'email', 'birthdate', 'phone_number')
     def not_empty(cls, v):
         if not v or not v.strip():
             raise ValueError('빈 값은 허용되지 않습니다.')
         return v
 
-    @field_validator('password2')
-    def passwords_match(cls, v, info: FieldValidationInfo):
-        if 'password1' in info.data and v != info.data['password1']:
-            raise ValueError('비밀번호가 일치하지 않습니다')
-        return v
-
     class Config:
-        from_attributes = True
+        use_enum_values = True
 
 
 class Token(BaseModel):
