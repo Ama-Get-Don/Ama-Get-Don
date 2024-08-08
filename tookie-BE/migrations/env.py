c8533bec755276f828b4c7479bcad8218cdab3bb
sangeun815 <grace0857@naver.com>
2024-08-09 06:32:43 +0900

    add birthdate & phone_number models.py

--- tookie-BE/migrations/env.py
+++ tookie-BE/migrations/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from models import Base
+import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- tookie-BE/user/user_crud.py
+++ tookie-BE/user/user_crud.py
@@ -1,16 +1,23 @@
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from user.user_schema import UserCreate
+from .user_schema import UserCreate
 from models import User
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_user(db: Session, user_create: UserCreate):
-    db_user = User(name=user_create.name,
-                   password=pwd_context.hash(user_create.password1),
-                   email=user_create.email)
+    db_user = User(
+        name=user_create.name,
+        password=pwd_context.hash(user_create.password),
+        email=user_create.email,
+        birthdate=user_create.birthdate, 
+        phone_number=user_create.phone_number,
+        gender=user_create.gender
+    )
     db.add(db_user)
     db.commit()
+    db.refresh(db_user)
+    return db_user
 
 def get_existing_user(db: Session, user_create: UserCreate):
     return db.query(User).filter(
