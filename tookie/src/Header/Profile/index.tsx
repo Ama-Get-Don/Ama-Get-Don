05747f70bfd77bda1e57cc76ba7352d7ca116dca
eowwo9 <owwo1@naver.com>
2024-08-11 05:31:08 +0900

    feat: modify profile

--- tookie/src/Header/Profile/index.tsx
+++ tookie/src/Header/Profile/index.tsx
@@ -1,20 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Avatar, Dropdown, Menu, Modal, Button } from 'antd';
-import { UserOutlined, InfoCircleOutlined, LogoutOutlined } from '@ant-design/icons';
+import { InfoCircleOutlined, LogoutOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import seedImage from '../../assets/profile_seed.png';
 import sproutImage from '../../assets/profile_sprout.png';
 import tookieImage from '../../assets/profile_tookie.png';
 import maleImage from '../../assets/profile_male.png';
+import femaleImage from '../../assets/profile_female.png';
+import seedOnImage from '../../assets/seed_on.png';
+import seedOffImage from '../../assets/seed_off.png';
+import sproutOnImage from '../../assets/sprout_on.png';
+import sproutOffImage from '../../assets/sprout_off.png';
+import tookieOnImage from '../../assets/tookie_on.png';
+import tookieOffImage from '../../assets/tookie_off.png';
+import arrowImage from '../../assets/Arrow.png';
 
 const Profile: React.FC = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [growthModalVisible, setGrowthModalVisible] = useState(false); // 새로운 모달 상태
+  const [selectedGender, setSelectedGender] = useState<string>('male');
+  const [avatarImage, setAvatarImage] = useState<string>(maleImage);
+  const [growthStage, setGrowthStage] = useState<string>('seed'); // 성장 단계 상태
   const navigate = useNavigate();
+
+  // 성별 정보 받아오는 useEffect
+  useEffect(() => {
+    const fetchGenderFromDB = async () => {
+      try {
+        const response = await fetch('/api/user/gender'); // API 엔드포인트 예시
+        const data = await response.json();
+        setSelectedGender(data.gender);
+      } catch (error) {
+        console.error('Error fetching gender:', error);
+      }
+    };
+
+    fetchGenderFromDB();
+  }, []);
+
+  // 성별에 따라 Avatar 이미지 설정 (성장 단계에 영향받지 않음)
+  useEffect(() => {
+    setAvatarImage(selectedGender === 'male' ? maleImage : femaleImage);
+  }, [selectedGender]);
 
   const handleMenuClick = (e: { key: string }) => {
     if (e.key === 'myInfo') {
-      setModalVisible(true); // 모달 열기
+      setModalVisible(true);
     } else if (e.key === 'logout') {
       navigate('/');
     }
@@ -22,6 +54,11 @@ const Profile: React.FC = () => {
 
   const handleCloseModal = () => {
     setModalVisible(false);
+  };
+
+  const handleGrowthStageChange = () => {
+    setGrowthModalVisible(false); // 모달 닫기
+    // 추가적으로 성장 단계 변경 후 서버로 상태를 전송하는 로직이 여기에 올 수 있음
   };
 
   const menu = (
@@ -32,6 +69,16 @@ const Profile: React.FC = () => {
     </StyledMenu>
   );
 
+  const getCurrentGrowthImage = () => {
+    if (growthStage === 'seed') {
+      return seedImage;
+    } else if (growthStage === 'sprout') {
+      return sproutImage;
+    } else if (growthStage === 'tookie') {
+      return tookieImage;
+    }
+  };
+
   return (
     <>
       <Dropdown
@@ -39,31 +86,77 @@ const Profile: React.FC = () => {
         trigger={['click']}
       >
         <ProfileAvatar>
-          <Avatar icon={<UserOutlined />} />
+          <Avatar src={avatarImage} />
         </ProfileAvatar>
       </Dropdown>
 
       <Modal
-        title="내 정보"
-        visible={modalVisible}
+        title={<span style={{ fontSize: '24px', fontWeight: 'bold' }}>내 정보</span>}
+        open={modalVisible}
         onCancel={handleCloseModal}
         footer={null}
+        width={600}
+      >
+        <Spacer />
+        <StyledModalContent>
+          <div className="current-stage-section" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+            <div>
+              <h3>현재 성장 단계</h3>
+              <GrowthImage src={getCurrentGrowthImage()} alt="현재 성장 단계" />
+            </div>
+            <StyledButton type="primary" shape="round" onClick={() => setGrowthModalVisible(true)} style={{ marginTop: '50px' }}>
+              성장 단계 직접 변경하기
+            </StyledButton>
+          </div>
+          <Spacer />
+          <div className="growth-stage-section">
+            <h3>캐릭터 성장 단계</h3>
+            <div className="growth-images">
+              <GrowthImage src={seedImage} alt="씨앗" style={{ width: '70px', height: '70px' }} />
+              <img src={arrowImage} alt="화살표" className="arrow-image" style={{ margin: '0 10px', alignSelf: 'center', transform: 'translateY(-20px)' }} />
+              <GrowthImage src={sproutImage} alt="새순" style={{ width: '70px', height: '70px' }} />
+              <img src={arrowImage} alt="화살표" className="arrow-image" style={{ margin: '0 10px', alignSelf: 'center', transform: 'translateY(-20px)' }}/>
+              <GrowthImage src={tookieImage} alt="투키" style={{ width: '70px', height: '70px' }} />
+            </div>
+          </div>
+          <Spacer />
+        </StyledModalContent>
+      </Modal>
+
+      <Modal
+        title={<span style={{ fontSize: '24px', fontWeight: 'bold' }}>성장 단계 변경하기</span>}
+        open={growthModalVisible}
+        onCancel={() => setGrowthModalVisible(false)}
+        footer={null}
+        width={600}
       >
         <StyledModalContent>
-          <ProfileImage src={maleImage} alt="Profile" />
-          <h3>현재 성장 단계</h3>
-          <GrowthImage src={maleImage} alt="현재 성장 단계" />
-          <h3>캐릭터 성장 단계</h3>
-          <div>
-            <GrowthImage src={seedImage} alt="씨앗" />
-            <span>→</span>
-            <GrowthImage src={sproutImage} alt="새순" />
-            <span>→</span>
-            <GrowthImage src={tookieImage} alt="투기" />
+          <h3>변경할 성장 단계를 선택하세요:</h3>
+          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <GrowthChooseImage
+              src={growthStage === 'seed' ? seedOnImage : seedOffImage}
+              alt="씨앗"
+              onClick={() => setGrowthStage('seed')}
+              style={{ cursor: 'pointer' }}
+            />
+            <GrowthChooseImage
+              src={growthStage === 'sprout' ? sproutOnImage : sproutOffImage}
+              alt="새순"
+              onClick={() => setGrowthStage('sprout')}
+              style={{ cursor: 'pointer' }}
+            />
+            <GrowthChooseImage
+              src={growthStage === 'tookie' ? tookieOnImage : tookieOffImage}
+              alt="투키"
+              onClick={() => setGrowthStage('tookie')}
+              style={{ cursor: 'pointer' }}
+            />
           </div>
-          <Button type="primary" style={{ margin: '10px' }}>
-            성장 단계 직접 변경하기
-          </Button>
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <StyledButton type="primary" shape="round" onClick={handleGrowthStageChange} style={{ marginTop: '20px' }}>
+              변경하기
+            </StyledButton>
+          </div>
         </StyledModalContent>
       </Modal>
     </>
@@ -74,6 +167,10 @@ export default Profile;
 
 const ProfileAvatar = styled.div`
   cursor: pointer;
+`;
+
+const Spacer = styled.div`
+    height: 25px;
 `;
 
 const StyledMenu = styled(Menu)`
@@ -97,13 +194,45 @@ const StyledMenuItem = styled(Menu.Item)`
 const StyledModalContent = styled.div`
 `;
 
-const ProfileImage = styled.img`
-  width: 50px;
-  height: 50px;
-`;
-
 const GrowthImage = styled.img`
   width: 50px;
   height: 50px;
   margin: 0 10px;
+`;
+
+const GrowthChooseImage = styled.img`
+  width: 150px;
+  height: 200px;
+  margin: 0 10px;
+`;
+
+const StyledButton = styled(Button)`
+  &&& {
+    margin: 10px 0;
+    width: 200px;
+    height: 40px;
+    font-size: 16px;
+    background-color: #00D282;
+    border-color: #00D282;
+    color: white;
+
+    &:hover, &:focus {
+      background-color: #00B870;
+      border-color: #00B870;
+      color: white;
+    }
+
+    &:active {
+      background-color: #009A5E;
+      border-color: #009A5E;
+    }
+
+    &:disabled {
+      background-color: #00D282;
+      border-color: #00D282;
+      color: white;
+      opacity: 0.7;
+      cursor: not-allowed;
+    }
+  }
 `;
